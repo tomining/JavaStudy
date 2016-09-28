9d3c9be3980126f7961c8c2b3730d49fbf843c2d
tomining <tomining83@gmail.com>
2016-09-28 21:31:54 +0900

    Exception Propagation

--- src/main/java/tomining/exception/ExceptionPropagationExample.java
+++ src/main/java/tomining/exception/ExceptionPropagationExample.java
@@ -4,4 +4,30 @@ package tomining.exception;
  * Created by NAVER on 2016. 7. 28..
  */
 public class ExceptionPropagationExample {
+    public static void main(String[] args) {
+        System.out.println("1. Main start");
+        ExceptionPropagationExample example = new ExceptionPropagationExample();
+
+        try {
+            example.doSomething();
+        } catch (Exception e) {
+            System.out.println("5. Catch exception");
+            e.printStackTrace();
+        }
+    }
+
+    private void doSomething() {
+        System.out.println("2. execute something");
+        try {
+            occursException();
+        } finally {
+            System.out.println("4. finally block");
+        }
+    }
+
+    private void occursException() {
+        System.out.println("3. Exception occurs");
+        String code = null;
+        code.length();  //NPE occurs
+    }
 }
